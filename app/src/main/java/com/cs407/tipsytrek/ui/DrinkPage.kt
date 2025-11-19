1f824977effe67acda698b147a1ab7a3f0fe3ec3
dseery <dseery@wisc.edu>
2025-11-19 17:03:01 -0600

    functional phys sim, first incomplete

--- app/src/main/java/com/cs407/tipsytrek/ui/DrinkPage.kt
+++ app/src/main/java/com/cs407/tipsytrek/ui/DrinkPage.kt
@@ -1,19 +1,19 @@
 package com.cs407.tipsytrek.ui
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import com.cs407.tipsytrek.Beverage
-import com.google.fpl.liquidfun.*
-
+import com.cs407.tipsytrek.sim.PhysicsSimulationScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -26,12 +26,12 @@ fun DrinkPage(beverage: Beverage, navController: NavController) {
             )
         }
     ) { innerPadding ->
-        Canvas(Modifier
+        Box(Modifier
             .fillMaxSize()
             .padding(innerPadding)) {
-            drawRect(Color(beverage.color))
-            val gravity: Vec2 = Vec2(0.0f, -9.8f)
-            //val world: World = World(gravity)
+            MaterialTheme {
+                PhysicsSimulationScreen()
+            }
         }
     }
 }
